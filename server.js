f361e009f38ee7e332d731a594a134025729c312
Man Esh <manishsheela902@gmail.com>
2023-07-06 10:08:17 +0530

    fixing cors issue

--- server.js
+++ server.js
@@ -3,12 +3,37 @@ require("dotenv").config();
 
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const connectDB = require('./config/db.js');
-const path = require('path');
+const connectDB = require("./config/db.js");
+const path = require("path");
 connectDB();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+// cors enable
+// const corsOptions = {
+//   // ["http://localhost:3000", "http://localhost:5000"],
+//   origin: process.env.ALLOWED_CLIENTS.split(","),
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Allow specific HTTP methods
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+  // Allow specific headers
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Allow credentials (if needed)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Continue to the next middleware
+  next();
+});
 
 // enable json 
 app.use(express.json())
